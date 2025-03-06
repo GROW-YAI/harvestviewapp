3f56f37754ccbd8d899bf55918e4aa1e46d26b06
Leticia Attoh <leticiaattoh1994@gmail.com>
2025-03-06 17:00:30 +0000

    worked it on smooth scrolling

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import ScrollToTop from "../components/ScrollToTop"; // ✅ Import ScrollToTop
+import ScrollToTop from "../components/ScrollToTop";
 
 const RootLayout = () => {
   return (
